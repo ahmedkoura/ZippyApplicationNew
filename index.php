fe0b189df7b351f3c10c49c0e0b497504a7c95ac
Ahmed Aboukoura <ahmedaboukoura1993@gmail.com>
2023-09-13 17:40:48 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,390 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Zippy</title>
+<link rel="icon" href="images/logo.png" />
+	
+<link href="css/slick.css" rel="stylesheet" />
+<link href="css/slick-theme.css" rel="stylesheet" />
+<link href="css/style.css?v=1" rel="stylesheet" />
+<style>
+	.slider {
+        margin: 70px 0px 0px 0px;
+		padding-bottom: 40px;
+    }
+
+    .slick-slide {
+      
+    }
+	.slides, .slick-slide div{
+		width: auto !important;
+		text-align: center;
+	}
+
+    .slick-slide img {
+      width: auto;
+		vertical-align: middle;
+    }
+
+    .slick-prev:before,
+    .slick-next:before {
+      color: black;
+    }
+
+
+    .slick-slide {
+      transition: all ease-in-out .3s;
+      opacity: .2;
+    }
+    
+    .slick-active {
+      opacity: 1;
+    }
+
+    .slick-current {
+      opacity: 1;
+    }
+</style>
+</head>
+
+<body>
+<div class="container">
+	<header id="home" class="sticky">
+		<div class="center">
+			<div class="headerTop text-center row">
+				<div class="logo left">
+					<a href="#home"><img src="images/logo.png" alt="logo"></a>
+				</div>
+				<div class="nav text-center">
+					<ul id="nav">   
+				   <li><a class="" href="#home">Home</a></li>
+				   <li><a class="" href="#partner">Partners</a></li>    
+				   <li><a class="" href="#about">About us</a></li>
+				   <li><a class="" href="#contact">Contact us</a></li>
+				  </ul>	 
+				</div>
+				<img src="images/menu.jpg" id="menu" style="display:none;"/>
+				<div class="navBtn right"><a href="#">Become a partner</a></div>
+			</div>
+			<div class="headerBottom text-center">
+				<div class="headerHeading row text-center">
+					<h1><span>Relief</span> is just <br /> a click away</h1>
+				</div>
+				<div class="headerContent row text-center">
+					<p>Looking for a sanitized clean bathroom while  you are on the move? look no further, Zippy offers numerous bathrooms for you to use,
+with special offers, discounts and much more!</p>
+				</div>
+				<div class="headerBtns row text-center">
+					<a href="#" class="storeBtns appleBtn"><img src="images/apple.png" /> App Store</a>
+					<a href="#" class="storeBtns playBtn"><img src="images/playstore.png" /> Play Store</a>
+				</div>
+			</div>
+		</div>
+	</header>
+	<section class="partnerSec" id="partner">
+		<div class="innerContainer">
+			<div class="row">
+				<div class="row secContent">
+					<p class="secTitle row">
+						Partners <img src="images/arrow.png" />
+					</p>
+					<p class="secHeading row text-center">Big names you can trust</p>
+					<p class="secDes row text-center">Clean bathrooms, offers and promotions, what’s not to like?</p>
+				</div>
+				
+				<div class="slider centeral row magic-display magic-top">
+					<div class="slides">
+					  <img src="images/slider/Rusty_Taco_logo 1.png" />
+					</div>
+					<div class="slides">
+					  <img src="images/slider/arbys.png" />
+					</div>
+					<div class="slides">
+					  <img src="images/slider/Burger_King_2020 1.png" />
+					</div>
+					<div class="slides">
+					  <img src="images/slider/Jacks_logo_2018 1.png" />
+					</div>
+					<div class="slides">
+					  <img src="images/slider/McDonald's_logo 1.png" />
+					</div>
+					
+					<div class="slides">
+					  <img src="images/slider/arbys.png" />
+					</div>
+					<div class="slides">
+					  <img src="images/slider/Burger_King_2020 1.png" />
+					</div>
+					<div class="slides">
+					  <img src="images/slider/Jacks_logo_2018 1.png" />
+					</div>
+					<div class="slides">
+					  <img src="images/slider/Rusty_Taco_logo 1.png" />
+					</div>
+					
+				
+				</div>
+				
+			</div>
+		</div>
+	</section>
+	
+	<section class="servicesSec" id="about">
+		<div class="innerContainer">
+			<div class="row">
+				<div class="row secContent">
+					<p class="secTitle row" style="padding-left: 27px;">
+						Services <img src="images/arrow.png" />
+					</p>
+					<p class="secHeading row text-center magic-display magic-bottom">More than a bathroom</p>
+				</div>
+				<div class="serviceContent row">
+					<div class="servicesIntro left row magic-display magic-left">
+						<p class="introTitle">
+							Big title goes here
+						</p>
+						<p class="introContent">
+							Lorem ipsum dolor sit amet consectetur. Tempus pellentesque turpis et ultrices bibendum eget suscipit leo etiam. Sapien duis quisque venenatis enim id nulla. Lectus euismod nisl scelerisque et. Purus quam vitae elementum scelerisque tincidunt in ac sagittis egestas. Ut sagittis nulla senectus vestibulum ultricies risus libero feugiat. Varius amet nascetur sed id mauris maecenas. Sed eu posuere laoreet gravida orci nibh sapien. Nulla amet turpis viverra non fames aliquam ultricies mauris aliquam. Tristique.
+						</p>
+						<p class="introContent">
+							Lorem ipsum dolor sit amet consectetur. Tempus pellentesque turpis et ultrices bibendum eget suscipit leo etiam. Sapien duis quisque venenatis enim id nulla. Lectus euismod nisl scelerisque et. Purus quam vitae elementum scelerisque tincidunt in ac sagittis egestas. Ut sagittis nulla senectus vestibulum ultricies risus libero feugiat. Varius amet nascetur sed id mauris maecenas. Sed eu posuere laoreet gravida orci nibh sapien. Nulla amet turpis viverra non fames aliquam ultricies mauris aliquam. Tristique.
+						</p>
+						<a href="#">
+							Explore All Services <img src="images/linkArrow.png" />
+						</a>
+						 
+					</div>
+					<div class="right serviceTabs row text-center">
+						<div class="serviceTab magic-display magic-right">
+							<div class="serviceIcon">
+								<img src="images/serviceIcon.png" />
+							</div>
+							<p class="serviceTitle row">Heading goes here</p>
+							<p class="serviceDes row">
+								Lorem ipsum dolor sit amet consectetur. Faucibus vitae varius in vitae malesuada aenean mauris egestas dictum. Congue vitae in mauris vitae semper. Tincidunt at nibh pellentesque elit urna. In id sollicitudin lorem sed id viverra eget pretium pellentesque.
+Cursus ipsum nam quisque.
+						
+							</p>
+						</div>
+						<div class="serviceTab magic-display magic-right">
+							<div class="serviceIcon">
+								<img src="images/serviceIcon.png" />
+							</div>
+							<p class="serviceTitle row">Heading goes here</p>
+							<p class="serviceDes row">
+								Lorem ipsum dolor sit amet consectetur. Faucibus vitae varius in vitae malesuada aenean mauris egestas dictum. Congue vitae in mauris vitae semper. Tincidunt at nibh pellentesque elit urna. In id sollicitudin lorem sed id viverra eget pretium pellentesque.
+Cursus ipsum nam quisque.
+						
+							</p>
+						</div>
+						<div class="serviceTab magic-display magic-right">
+							<div class="serviceIcon">
+								<img src="images/serviceIcon.png" />
+							</div>
+							<p class="serviceTitle row">Heading goes here</p>
+							<p class="serviceDes row">
+								Lorem ipsum dolor sit amet consectetur. Faucibus vitae varius in vitae malesuada aenean mauris egestas dictum. Congue vitae in mauris vitae semper. Tincidunt at nibh pellentesque elit urna. In id sollicitudin lorem sed id viverra eget pretium pellentesque.
+Cursus ipsum nam quisque.
+						
+							</p>
+						</div>
+						<div class="serviceTab magic-display magic-right">
+							<div class="serviceIcon">
+								<img src="images/serviceIcon.png" />
+							</div>
+							<p class="serviceTitle row">Heading goes here</p>
+							<p class="serviceDes row">
+								Lorem ipsum dolor sit amet consectetur. Faucibus vitae varius in vitae malesuada aenean mauris egestas dictum. Congue vitae in mauris vitae semper. Tincidunt at nibh pellentesque elit urna. In id sollicitudin lorem sed id viverra eget pretium pellentesque.
+Cursus ipsum nam quisque.
+						
+							</p>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</section>
+	
+	<section class="locationsSec">
+		<div class="innerContainer">
+			<div class="row">
+				<div class="row secContent locationTabContent">
+					<p class="secTitle row" style="padding-left: 27px;">
+						Locations <img src="images/arrow.png" />
+					</p>
+					<p class="secHeading row text-center">Premium Locations & offers</p>
+				</div>
+				<div class="locationTabs row text-center">
+					<div class="locationTab magic-display magic-left">
+						<img src="images/locationImg.png" />
+						<p class="locationTitle serviceTitle text-center">Location Name</p>
+						<p class="locationDec text-center">
+							Lorem ipsum dolor sit amet consectetur. Sit semper duis pellentesque cursus feugiat. Donec quam eget massa posuere eget massa tincidunt est. Amet sodales non cursus gravida vivamus dui quis. Placerat urna auctor tortor orci. Tortor viverra nunc senectus consequat posuere ut accumsan semper in. Lobortis nunc aliquam platea sit mauris laoreet volutpat.
+						</p>
+					</div>
+					<div class="locationTab magic-display magic-top">
+						<img src="images/locationImg.png" />
+						<p class="locationTitle serviceTitle text-center">Location Name</p>
+						<p class="locationDec text-center">
+							Lorem ipsum dolor sit amet consectetur. Sit semper duis pellentesque cursus feugiat. Donec quam eget massa posuere eget massa tincidunt est. Amet sodales non cursus gravida vivamus dui quis. Placerat urna auctor tortor orci. Tortor viverra nunc senectus consequat posuere ut accumsan semper in. Lobortis nunc aliquam platea sit mauris laoreet volutpat.
+						</p>
+					</div>
+					<div class="locationTab magic-display magic-right">
+						<img src="images/locationImg.png" />
+						<p class="locationTitle serviceTitle text-center">Location Name</p>
+						<p class="locationDec text-center">
+							Lorem ipsum dolor sit amet consectetur. Sit semper duis pellentesque cursus feugiat. Donec quam eget massa posuere eget massa tincidunt est. Amet sodales non cursus gravida vivamus dui quis. Placerat urna auctor tortor orci. Tortor viverra nunc senectus consequat posuere ut accumsan semper in. Lobortis nunc aliquam platea sit mauris laoreet volutpat.
+						</p>
+					</div>
+					
+				</div>
+				
+			</div>
+		</div>
+	</section>
+	
+	<section class="contactSec" id="contact">
+		
+		
+		<div class="formSec half row">
+			<div class="innerContainer">
+				<div class="row">
+					<p class="secTitle contactTitle row">
+							Contact us <img src="images/arrow.png" />
+					</p>
+					<p class="secHeading formHeading row text-left">We Would love to <br />hear from you</p>
+					<form method="post" action="#">
+						<label>Full Name</label>
+						<input type="text" class="formField magic-display magic-bottom" name="name" />
+						<label>Email</label>
+						<input type="email" class="formField magic-display magic-bottom" name="email" />
+						<label>Full Name</label>
+						<input type="text" class="formField magic-display magic-bottom" name="phone" />
+						<label>Message</label>
+						<textarea class="formField textarea magic-display magic-bottom" name="message"></textarea>
+						<button type="submit" class="formBtn magic-display magic-bottom" name="send">Send</button>
+					</form>
+				</div>
+			</div>
+		</div>
+		
+		<div class="formSec followSec half row">
+			<div class="innerContainer">
+				<div class="row">
+					<p class="secHeading formHeading followHeading row text-left">Follow our <br />Socials</p>
+					<div class="row socialIcons text-left">
+						<a href="#" class="magic-display magic-bottom"><img src="images/instagram.png" /></a>
+						<a href="#" class="magic-display magic-bottom"><img src="images/facebook.png" /></a>
+						<a href="#" class="magic-display magic-bottom"><img src="images/twitter.png" /></a>
+					</div>
+					<p class="secHeading formHeading followHeading getintouchHeading row text-left">Get in touch</p>
+					<p class="contactItems row"><img src="images/mail.png" /> info@zippy.com</p>
+					<p class="contactItems row"><img src="images/phone.png" /> XXX-XXX-XXXXXX</p>
+					<p class="secHeading formHeading followHeading findHeading row text-left">Find us here</p>
+					<img src="images/findusImg.png" class="mapImg magic-display magic-top" />
+				</div>
+			</div>
+		</div>
+		
+	</section>
+	<footer class="text-center">
+		All rights reserved, Zippy 2023
+	</footer>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+ 
+<script type="text/javascript">
+	$( "#menu" ).click( function () {
+	$( "#nav" ).toggle( "slow", "swing" );
+	} );
+</script>
+	
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" type="text/javascript"></script>
+<script src="js/slick.js"></script>
+<script>
+$(".centeral").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 3,
+		autoplay:false,
+		autoplaySpeed: 3000,
+		arrows: true,
+	 prevArrow:'<button type="button" data-role="none" class="slick-prev"><img src="images/larrow.png" /></button>',
+	nextArrow:'<button type="button" data-role="none" class="slick-next"><img src="images/rarrow.png" /></button>',
+	dots:false,
+	slidesToShow: 5,
+        slidesToScroll: 1,
+	responsive: [{
+
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 2,
+      infinite: true
+    }
+
+  }, {
+
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 1
+    }
+
+  }, {
+
+    breakpoint: 300,
+    settings: "unslick" // destroys slick
+
+  }]
+
+
+		
+      });
+</script>
+ <script type="text/javascript" src="https://www.jqueryscript.net/demo/jQuery-Plugin-For-Entrance-Animations-Of-Elements-Magic-Display/jquery.magic.display.min.js"></script>
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('.magic-display').magicDisplay({
+      duration: 500,
+      displayFullVisible: true
+    });
+  });
+  </script>
+ 
+  <script>
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+	
+
+});
+</script>
+</body>
+</html>
